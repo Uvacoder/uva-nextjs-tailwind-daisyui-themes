5bb6e36107ee49ddb8beb4533d59f4b8d47b6af9
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-05-26 13:07:39 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'next/core-web-vitals',
+    'plugin:@next/next/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next',
+  ],
+}
---
+++ components/ThemeChanger/ThemeChanger.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes'
+const allThemes = [
+  'light',
+  'dark',
+  'cupcake',
+  'bumblebee',
+  'emerald',
+  'corporate',
+  'synthwave',
+  'retro',
+  'cyberpunk',
+  'valentine',
+  'halloween',
+  'garden',
+  'forest',
+  'aqua',
+  'lofi',
+  'pastel',
+  'fantasy',
+  'wireframe',
+  'black',
+  'luxury',
+  'dracula',
+  'cmyk',
+  'autumn',
+  'business',
+  'acid',
+  'lemonade',
+  'night',
+  'coffee',
+  'winter',
+]
+
+const themes = allThemes.map((t_name) => ({ name: t_name }))
+
+const ThemeChanger = () => {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), [])
+
+  if (!mounted) return null
+
+  return (
+    <div className="p-4 flex justify-between items-center font-bold text-xl bg-secondary text-secondary-content rounded-md space-x-4">
+      <span>
+        The current theme is: <strong>{theme}</strong>
+      </span>
+      <div>
+        <label htmlFor="theme-select" className="sr-only mr-2">
+          Choose theme:
+        </label>
+        <select
+          name="theme"
+          id="theme-select"
+          className="bg-white text-gray-800 border-gray-800 border py-1 px-3"
+          onChange={(e) => setTheme(e.currentTarget.value)}
+          value={theme}
+        >
+          <option value="">Select Theme</option>
+          {themes.map((t) => (
+            <option key={t.name.toLowerCase()} value={t.name.toLowerCase()}>
+              {t.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  )
+}
+
+export default ThemeChanger
